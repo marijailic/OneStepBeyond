2912d0c30481da2d7a28019b67a5ef6aeaa21a27
marijailic <marija.cbsqg@gmail.com>
2022-09-06 00:25:36 +0200

    email i pretraga rade

--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 from flask import request, make_response, jsonify, render_template, Blueprint
-from db_connector import get_koncerti, get_koncert
+from db_connector import get_koncerti, get_koncert, search_koncerti
+from send_email import send_email
 
 
 user = Blueprint("user", __name__)
@@ -34,3 +35,41 @@ def podaci_koncert(id):
         return make_response(render_template("user-koncert.html", data=response["data"]), 200)
     else:
         return make_response(jsonify(response), 400)
+
+
+# slanje e-maila
+
+
+@user.route("/user/potvrda/<int:id>", methods=["POST", "GET"])
+def slanje_potvrde(id):
+    response = get_koncert(id)
+    if response["response"] == "Uspjesno dohvacanje!":
+        data = response["data"]
+        send_email(data)
+        return make_response(jsonify(response), 200)
+    else:
+        return make_response(jsonify(response), 400)
+
+
+# pretraga
+
+@user.route("/user/pretraga", methods=["POST", "GET"])
+def pretrazi_koncerte():
+    if request.method == "POST":
+        try:
+            json_request = {}
+            for key, value in request.form.items():
+                if value == '':
+                    json_request[key] = None
+                else:
+                    json_request[key] = value
+        except Exception as e:
+            response = {"response": str(e)}
+            return make_response(jsonify(response), 400)
+        response = search_koncerti(json_request)
+        if response["response"] == "Uspjesno dohvacanje!":
+            return make_response(render_template("user-koncert.html", data=response["data"]), 200)
+        else:
+            return make_response(jsonify(response), 400)
+    else:
+        return make_response(render_template("user-pretraga.html"), 200)
